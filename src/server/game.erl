52ec491bcaabdc533639484a25b826a463612db0
Tiago Teixeira <a97666@alunos.uminho.pt>
2024-05-24 19:25:15 +0100

    advancing to match (generating random planets)

--- src/server/game.erl
+++ src/server/game.erl
@@ -10,51 +10,101 @@
 start() ->
 	spawn(fun() -> game([]) end).
 
-game(Players) when length(Players) < 2 ->
+
+loop(State) ->
 	receive
-		{join_game, Player} ->
+		{Request, From} ->
+			{Result, NextState} = handle(Request, State),
+			From ! {Result, ?MODULE},
+			loop(NextState)
+	end.
+
+game(PlayersPids) when length(PlayersPids) < 2 ->
+	receive
+		{join_game, UserPid} ->
             ?MODULE ! {more_players},
-			game([Player | Players]);
+			game([UserPid | PlayersPids]);
 	end;
 
-game(Players) when ((length(Players) >= 2) and (length(Players) < 4)) ->
+game(PlayersPids) when ((length(PlayersPids) >= 2) and (length(PlayersPids) < 4)) ->
 	Game = self(),
 	Timer = spawn(fun() -> receive after 5000 -> Game ! timeout end end),
 	receive
 		timeout ->
-			Match = spawn(fun() -> match(Players) end),
-			[ Player ! {Match, self()} || Player <- Players],
+            Participants = through_players(PlayersPids),
+			Match = spawn(fun() -> match(Participants) end),
+			[ PlayerPid ! {Match, self()} || PlayerPid <- PlayersPids],
 			game([]);
-		{join_game, Player} ->
+
+		{join_game, PlayerPid} ->
             exit(Timer,kill),
-			game([Player | Players]);
+			game([PlayerPid | PlayersPids]);
             
 	end;
 
-game(Players) ->
-	Match = spawn(fun() -> match(Players) end),
-	[ Player ! {Match, self()} || Player <- Players],
+game(PlayersPids) ->
+    Participants = through_players(PlayersPids),
+	Match = spawn(fun() -> match(Participants) end),
+
+	[ PlayerPid ! {Match, self()} || PlayerPid <- PlayersPids],
 	game([]).
+
 % FIXME corrEct API usagE in TCP sErvEr
 
-join_game(UserPid) ->
-    ?MODULE ! {join_game, UserPid},
+join_game(PlayerPid) ->
+    ?MODULE ! {join_game, PlayerPid},
     receive
-        {start} -> "Starting"
-    end;
-
-keyPressed(Key, UserPid) -> 
-    ?MODULE ! {key, Key, } 
-
+        {start} -> "Starting" end;
+keyPressed(Key, PlayerPid) -> 
+    ?MODULE ! {Request, Key},
+    
 del_game() ->
 	ok.
 stop_game() ->
 	ok.
 
 %-----------------------------MATCH------------------------------
-match([Player | Players]) ->
-    	
+match(Map) ->
+    
 	% TODO game logic should b3 impl3m3nt3d
 
 
 % TODO do this
+
+%-----------------------FUNCOES AUXILIARES----------------------
+generate_planets(Int) -> 
+
+    case Int of -> 
+        0 ->
+
+
+
+
+through_players(List) ->
+    Planets = generate_planets(random:uniform(5)),
+    Map = maps:new(),
+    Map#{hd(List) => {x,y,10,
+                      targetX,targetY,angle,
+                      lineLength,lineEndX,lineEndY,
+                      targetAngle,easingAngle,
+                      r,g,b,
+                      100,
+                      false,true,false
+                     }
+        },
+    through_players(tl(List),Map);
+
+through_players(List,Map) when length(List) > 0 ->    
+    Map#{hd(List) => {x,y,10,
+                      targetX,targetY,angle,
+                      lineLength,lineEndX,lineEndY,
+                      targetAngle,easingAngle,
+                      r,g,b,
+                      100,
+                      false,true,false
+                     }
+        },
+    through_players(tl(List),Map);
+
+through_players([],Map) -> 
+    Map;
