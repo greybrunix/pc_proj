885c64e161edc9a0e70c38d52c92df227f0fc3d4
Tiago Teixeira <a97666@alunos.uminho.pt>
2024-05-26 21:05:40 +0100

    funções auxiliares para colocar planetas e players no sitio feito, mas ver nuances

--- src/server/game.erl
+++ src/server/game.erl
@@ -66,44 +66,70 @@ stop_game() ->
 %-----------------------------MATCH------------------------------
 match(Map) ->
     
+    Planets = generate_planets(randomNumRange(2,5)),
 	% TODO game logic should b3 impl3m3nt3d
 
 
 % TODO do this
 
 %-----------------------FUNCOES AUXILIARES----------------------
+randomNumRange(Small,Big) ->
+    random:uniform(Big - Small + 1) + Small - 1;
+
 generate_planets(Int) -> 
+    Sistema = Map#{0 => {0,960,540,35,255,255,0}}, % here comes the sun
+    generate_planets(Int, Sistema);
 
-    case Int of -> 
-        0 ->
+generate_planets(Int,Sistema) ->
+    case Int of ->
+         0 ->
+            Sistema;
+         _ -> 
+            Distancia = Int * randomNumRange(40,100),
+            % Ver se vale a pena colocar distancia sol-planeta
+            Sistema = maps:put(Int,{Distancia,
+                                    randomNumRange(300,1600),
+                                    randomNumRange(200,850),
+                                    randomNumRange(4,20),
+                                    randomNumRange(90,255),
+                                    randomNumRange(90,255),
+                                    randomNumRange(90,255)}),
+            generate_planets(Int-1,Sistema);
 
 
+newPlayerPos(Player,Map) ->
+    X = randomNumRange(300,1600),
+    Y = randomNumRange(200,850),
+    if
+        X >= 960 -> X = 1750;
+        X < 960 -> X = 150;
+    end,
 
+    if
+        Y >= 540 -> Y = 900;
+        Y < 540 -> Y = 100;
+    end,
+
+    Map = maps:put(Player,{X,Y,5,
+                           X,Y,0,
+                           15,X+math:cos(0) * 15,Y+math:sin(0) * 15,
+                           0,0.2,
+                           randomNumRange(90,255),
+                           randomNumRange(90,255),
+                           randomNumRange(90,255),
+                           100,
+                           false,true,false
+                          }
+                   Map),
+    Map;
 
 through_players(List) ->
-    Planets = generate_planets(random:uniform(5)),
     Map = maps:new(),
-    Map#{hd(List) => {x,y,10,
-                      targetX,targetY,angle,
-                      lineLength,lineEndX,lineEndY,
-                      targetAngle,easingAngle,
-                      r,g,b,
-                      100,
-                      false,true,false
-                     }
-        },
+    newPlayerPos(hd(List),Map),
     through_players(tl(List),Map);
 
 through_players(List,Map) when length(List) > 0 ->    
-    Map#{hd(List) => {x,y,10,
-                      targetX,targetY,angle,
-                      lineLength,lineEndX,lineEndY,
-                      targetAngle,easingAngle,
-                      r,g,b,
-                      100,
-                      false,true,false
-                     }
-        },
+    newPlayerPos(hd(List),Map),
     through_players(tl(List),Map);
 
 through_players([],Map) -> 
